9bf73daac3df84e54e8c477975291e51e17ad967
Aiden Swann <swannaiden@gmail.com>
2020-12-19 14:23:39 -0600

    added pyinstaller fix

--- layout.py
+++ layout.py
@@ -1,3 +1,26 @@
+import sys
+from collections import namedtuple
+
+import pkg_resources
+
+IS_FROZEN = hasattr(sys, '_MEIPASS')
+
+# backup true function
+_true_get_distribution = pkg_resources.get_distribution
+# create small placeholder for the dash call
+# _flask_compress_version = parse_version(get_distribution("flask-compress").version)
+_Dist = namedtuple('_Dist', ['version'])
+
+def _get_distribution(dist):
+    if IS_FROZEN and dist == 'flask-compress':
+        return _Dist('1.8.0')
+    else:
+        return _true_get_distribution(dist)
+
+# monkey patch the function so it can work once frozen and pkg_resources is of
+# no help
+pkg_resources.get_distribution = _get_distribution
+
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc 
@@ -573,4 +596,4 @@ if __name__ == '__main__':
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
         webbrowser.open_new('http://127.0.0.1:8050/')
     
-    app.run_server(debug=True)
+    app.run_server(debug=False)
